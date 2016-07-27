0fa72f31801654c9ff94b1a460d3b98d1c3f5470
MattRijk <mattrijk11@gmail.com>
2016-07-27 20:27:00 +0000

    Added child theme and added to layout.

---
+++ wp-content/themes/rsquared-child/front-page.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The main template file.
+ *
+ * @package WordPress
+ * @subpackage Starkers
+ * @since Starkers HTML5 3.0
+ */
+ 
+get_header(); ?>
+ 
+
+<section class="main">
+    <aside>
+        <div class="content">
+            <h3><a href="http://www.marketwatch.com/story/10-good-things-that-have-happened-in-2016-2016-07-27">10 good things that have happened in 2016</a></h3>
+            <p>Yes, according to a recent tally from the World Wildlife Fund, there are now 3,890 tigers in the world, up from about 3,200 in 2010. This marks the first time 
+            in a century the population is growing. WWF’s goal is to double the number of wild tigers by 2022.</p>  
+        </div>
+    </aside>
+    
+    <aside>
+        <div class="content">
+            <h3><a href="http://www.marketwatch.com/tools/stockresearch/screener/afterhours.asp">After-hours winners, losers</a></h3>
+            <p>Investors in stock index funds have had a good run as most actively managed funds have lagged behind their benchmarks. But that may 
+            change, as record-high equity indices favor skilled stock pickers.</p>  
+        </div>
+    </aside>
+    
+    <aside>
+        <div class="content">
+            <h3><a href="http://www.cnn.com/2015/11/03/world/cnnphotos-salgado-coffee-farming/index.html">A look inside the world of coffee</a></h3>
+            <p>The aroma of coffee is invisible to the human eye, but certainly not to the nose. The reality is that much of the world of coffee is like the 
+            aroma of it -- familiar, yet unknown, hidden from plain sight.</p>   
+        </div>
+    </aside>
+    
+</section>
+<section class="atmosphere">
+    <article>
+    	<h2>Creating a modern atmosphere</h2>
+    	<p>Released in March this year, Adobe Experience Design is a new all-in-one tool that lets you design and prototype websites and mobile apps. for websites and mobile apps. XD is still in beta and available for Mac with a Windows version on track for a release later in 2016.</p>
+    	<a class="btn" title="Creating a modern atmosphere" href="#">Learn more</a>
+    </article>
+    
+</section>
+<section class="how-to"></section>
+
+<?php get_footer(); ?>
